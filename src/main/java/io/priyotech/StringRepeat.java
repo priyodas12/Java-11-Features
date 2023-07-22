72625b73158297e4274e7d4d1c8d52de7afe0abd
Priyobrato.Das <priyodas12@gmail.com>
2023-07-22 15:20:55 +0530

    string repeat

---
+++ src/main/java/io/priyotech/StringRepeat.java
@@ -0,0 +1,9 @@
+package io.priyotech;
+
+public class StringRepeat {
+    public static void main(String[] args) {
+        String hashSign="#";
+        System.out.println(hashSign.repeat(10));
+        System.out.println(hashSign.repeat(0));//empty string
+    }
+}
