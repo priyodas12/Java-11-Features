4a0244565685df20b179eb36e5333672b363fa6e
Priyobrato.Das <priyodas12@gmail.com>
2024-05-09 10:30:24 +0530

    added logger and reformat code

--- src/main/java/io/priyotech/HttpClientAPIImpl.java
+++ src/main/java/io/priyotech/HttpClientAPIImpl.java
@@ -5,33 +5,49 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
 public class HttpClientAPIImpl {
-    static final String endPoint = "http://www.boredapi.com/api/activity?participants=1";
 
-    public static void main(String[] args) {
-        HttpResponse<String> response = null;
-        try {
-            HttpClient httpClient = HttpClient.newBuilder()
-                    .connectTimeout(Duration.ofSeconds(2))
-                    .build();/**builder pattern in action*/
+  private static final Logger logger = Logger.getLogger(HttpClientAPIImpl.class.getName());
+  private static final String endPoint = "http://www.boredapi.com/api/activity?participants=1";
 
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(endPoint))
-                    .GET()
-                    .build();/**builder pattern in action*/
+  public static void main(String[] args) {
+    fetchApi();
+  }
 
-            /**BodyHandlers converts response into string*/
-            response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (response != null) {
-                System.out.println(response.body());
-            } else {
-                System.out.println("No Response from boredApi...");
-            }
-        }
+  private static void fetchApi() {
+    HttpResponse<String> response = null;
+    String responseBody = "";
+    try {
+      HttpClient httpClient = HttpClient.newBuilder()
+          .connectTimeout(Duration.ofSeconds(1))
+          .build(); /**builder pattern in action*/
+
+      HttpRequest httpRequest = HttpRequest.newBuilder()
+          .uri(URI.create(endPoint))
+          .GET()
+          .build(); /**builder pattern in action*/
+
+      /**BodyHandlers converts response into string*/
+      response = httpClient.send(httpRequest, BodyHandlers.ofString());
+      responseBody = response.body();
+    } catch (IOException | InterruptedException e) {
+      logger.log(Level.INFO, e.getMessage());
+      throw new RuntimeException(e);
+    } finally {
+      if (response.statusCode() == 200) {
+        logger.log(Level.INFO, responseBody);
+      } else {
+        logger.info("No Response from boredApi...");
+      }
     }
+  }
 }
